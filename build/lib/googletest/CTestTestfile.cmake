b63ef34f8bbf74bd56a6cee7eeac5f07caa5843a
Przemyslaw Skupien <przemyslaw.skupien@gmail.com>
2020-11-11 10:43:46 +0100

    second commit

--- build/lib/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/przemek/dev/2dgameengine/lib/googletest
-# Build directory: /home/przemek/dev/2dgameengine/build/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/src/CMakeFiles/2dgame_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "lib2dgame_lib.a"
-)
